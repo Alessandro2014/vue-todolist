efbbd6f3656eb85b102f2d63663ca5d09cdf2b93
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-29 16:42:46 +0200

    errore newTask

--- js/script.js
+++ js/script.js
@@ -1,16 +1,24 @@
 const root = new Vue({
     el: '#root',
     data: {
+        newTask:'',
         tasks:[
             'Fare le valigie',
             'Comprare pane',
             'Abbeverare piante',
             'Ordinare il dolce',
+            'Lavare auto',
         ], 
     },
     methods: {
        deletedTask(i){
            this.tasks.splice(i, 1);
-       }
+       },
+       addTask() {
+        if(this.newTask.trim()){
+            this.tasks.push(newTask);
+        }
+        this.newTask = '';
+       },
     },
 });
