41af9a9c32f544470bb92ebd329c13962b0d661f
Alessandro2014 <alessandromerenda22@gmail.com>
2021-07-29 15:46:45 +0200

    stampa lista

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const root = new Vue({
+    el: '#root',
+    data: {
+        tasks:[
+            'Fare le valigie',
+            'Comprare pane',
+            'Abbeverare piante',
+            'Ordinare il dolce',
+        ], 
+    },
+    methods: {
+       
+    },
+});
